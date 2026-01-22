10d1069a1466e883c239ec831564ef1f1ba0bd7c
dhirajsen2505-ui <dhirajsen2505@gmail.com>
2026-01-22 22:38:15 +0530

    web.py

--- web.py
+++ web.py
@@ -1,14 +1,12 @@
 import  streamlit as st
 
 name = st.text_input("Enter your name : ")
-fname = st.text_input("Enter your father name : ")
 adr = st.text_area("Enter your address : ")
-classdata = st.selectbox("Enter Your Semester :",(1,2,3,4,5,6,7,8))
+quantity = st.selectbox("Enter How Many Water Bottel You Need :",(50,100,150,200,300,400,500,1000,1500))
 
 button = st.button("Submit")
 if button:
     st.markdown(f"""
     Name: {name}
-    Father: {fname}
     Address: {adr}
-    classdata: {classdata}""")
+    quantity: {quantity}""")
